5598a25659450a960e0d5c2acb3e7ef5028ade31
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-04-29 14:48:25 +0530

    added bosch invites quick report

---
+++ cypress/e2e/reportDownloads/boschInvitesQuickReport/bosch_invites_report_of_today.cy.js
@@ -0,0 +1,29 @@
+import LoginPage from "../../../pageObject/login.js";
+import QuickReport from "../../../pageObject/quick_report.js";
+const quickReport = new QuickReport();
+const loginPage = new LoginPage()
+
+describe('Portal Downloads : quick reports  ', () => {
+    before( () =>{ 
+        cy.fixture('loginPage.json').then((userdata)=>{
+            cy.visit(Cypress.env('url'))
+            loginPage.getEmailInputField(userdata.adminEmail);
+            loginPage.getPasswordInputField(userdata.adminPassword);
+            loginPage.getsubmitBtn();
+            loginPage.getTextVerifyLogin();
+        })
+    })
+
+    it('should show confirmation message of quick report  after clicking on download for button of Bosch Invites Report with today',()=>{
+        cy.wait(5000)
+        quickReport.getClickOnDownloadCenter();
+        quickReport.getClickOnQuickReport();
+        quickReport.verifyTitleText().eq(17).should("contain.text", "Bosch Invites Report")
+        quickReport.getClickOnDatePicker().eq(13).click();
+        quickReport.getClickOnToday();
+        cy.wait(3000)
+        quickReport.getClickOnDownloadForBtn().eq(13).click();
+        quickReport.verifySuccessMessage();
+        
+    })
+})
