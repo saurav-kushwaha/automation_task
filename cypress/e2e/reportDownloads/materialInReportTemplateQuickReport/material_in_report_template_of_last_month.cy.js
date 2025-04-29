4cfe38121165103d5215983a04a0b06452bc2643
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-04-29 16:02:04 +0530

    added material in quick report

--- cypress/e2e/reportDownloads/materialInReportTemplateQuickReport/material_in_report_template_of_last_month.cy.js
+++ cypress/e2e/reportDownloads/materialInReportTemplateQuickReport/material_in_report_template_of_last_month.cy.js
@@ -14,14 +14,14 @@ describe('Portal Downloads : quick reports  ', () => {
         })
     })
 
-    it('should show confirmation message of quick report  after clicking on download for button of This report is used for MRM with last week',()=>{
+    it('should show confirmation message of quick report  after clicking on download for button of Material In Report Template with last month',()=>{
         cy.wait(5000)
         quickReport.getClickOnDownloadCenter();
         quickReport.getClickOnQuickReport();
-        quickReport.verifyTitleText().eq(19).should("contain.text", "This report is used for MRM")
-        quickReport.getClickOnDatePicker().eq(15).click();
-        quickReport.getClickOnLastWeek();
-        quickReport.getClickOnDownloadForBtn().eq(15).click();
+        quickReport.verifyTitleText().eq(20).should("contain.text", "Material In Report Template")
+        quickReport.getClickOnDatePicker().eq(16).click();
+        quickReport.getClickOnLastMonth();
+        quickReport.getClickOnDownloadForBtn().eq(16).click();
         quickReport.verifySuccessMessage();
         
     })
---
+++ cypress/e2e/reportDownloads/materialInReportTemplateQuickReport/material_in_report_template_of_today.cy.js
@@ -0,0 +1,30 @@
+import LoginPage from "../../../pageObject/login.js";
+import QuickReport from "../../../pageObject/quick_report.js";
+const quickReport = new QuickReport();
+const loginPage = new LoginPage()
+
+describe('Portal Downloads : quick reports  ', () => {
+    before( () =>{ 
+        cy.fixture('loginPage.json').then((userdata)=>{
+            cy.visit(Cypress.env('url'))
+            loginPage.getEmailInputField(userdata.adminEmail);
+            loginPage.getPasswordInputField(userdata.adminPassword);
+            loginPage.getsubmitBtn();
+            loginPage.getTextVerifyLogin();
+        })
+    })
+
+    it('should show confirmation message of quick report  after clicking on download for button of Material In Report Template with today',()=>{
+        cy.wait(5000)
+        quickReport.getClickOnDownloadCenter();
+        quickReport.getClickOnQuickReport();
+        quickReport.verifyTitleText().eq(20).should("contain.text", "Material In Report Template")
+        quickReport.getClickOnDatePicker().eq(16).click();
+        quickReport.getClickOnToday();
+        cy.wait(3000)
+        quickReport.getClickOnToday();
+        quickReport.getClickOnDownloadForBtn().eq(16).click();
+        quickReport.verifySuccessMessage();
+        
+    })
+})
