284a0adbcbeb9c7f6adc9e6c15789ec93b1cc9b1
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-04-29 13:20:08 +0530

    quick report of past visitor audit

---
+++ cypress/e2e/reportDownloads/PastVisitorAuditReport/past_visitor_audit_report_with_this_week.cy.js
@@ -0,0 +1,28 @@
+import LoginPage from "../../../pageObject/login.js";
+import QuickReport from "../../../pageObject/quick_report.js";
+const quickReport = new QuickReport();
+const loginPage = new LoginPage()
+
+describe('Portal Downloads : quick reports  ', () => {
+    before( () =>{ 
+        cy.fixture('loginPage.json').then((userdata)=>{
+            cy.visit(Cypress.env('url'))
+            loginPage.getEmailInputField(userdata.adminEmail);
+            loginPage.getPasswordInputField(userdata.adminPassword);
+            loginPage.getsubmitBtn();
+            loginPage.getTextVerifyLogin();
+        })
+    })
+
+    it('should show confirmation message of quick report  after clicking on download for button of Past Visitor Audit Report with this week',()=>{
+        cy.wait(5000)
+        quickReport.getClickOnDownloadCenter();
+        quickReport.getClickOnQuickReport();
+        quickReport.verifyTitleText().eq(11).should("contain.text", "Past Visitor Audit Report")
+        quickReport.getClickOnDatePicker().eq(7).click();
+        quickReport.getClickOnThisWeek();
+        quickReport.getClickOnDownloadForBtn().eq(7).click();
+        quickReport.verifySuccessMessage();
+        
+    })
+})
