8b514ba1abcf5f4cd798874e08b626418de71b1d
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-04-29 13:20:58 +0530

    quick report of visitor

---
+++ cypress/e2e/reportDownloads/visitorReport/visitor_report_with_last_month.cy.js
@@ -0,0 +1,28 @@
+import LoginPage from "../../../pageObject/login.js";
+import QuickReport from "../../../pageObject/quick_report.js";
+const quickReport = new QuickReport();
+const loginPage = new LoginPage()
+
+describe('Portal Downloads : quick reports  ', () => {
+    before( () =>{ 
+        cy.fixture('loginPage.json').then((userdata)=>{
+            cy.visit(Cypress.env('url'))
+            loginPage.getEmailInputField(userdata.adminEmail);
+            loginPage.getPasswordInputField(userdata.adminPassword);
+            loginPage.getsubmitBtn();
+            loginPage.getTextVerifyLogin();
+        })
+    })
+
+    it('should show confirmation message of quick report  after clicking on download for button of Visitor Report with last month',()=>{
+        cy.wait(5000)
+        quickReport.getClickOnDownloadCenter();
+        quickReport.getClickOnQuickReport();
+        quickReport.verifyTitleText().eq(12).should("contain.text", "Visitor Report")
+        quickReport.getClickOnDatePicker().eq(8).click();
+        quickReport.getClickOnLastMonth();
+        quickReport.getClickOnDownloadForBtn().eq(8).click();
+        quickReport.verifySuccessMessage();
+        
+    })
+})
