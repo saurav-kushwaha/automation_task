8e60c2f24a1b6fadbe058dc1853ac37c7e22b19f
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-04-29 15:11:40 +0530

    add this report is used for mrm

---
+++ cypress/e2e/reportDownloads/thisReportIsUsedForMRM/this_report_is_used_for_mrm_of_this_month.cy.js
@@ -0,0 +1,28 @@
+import LoginPage from "../../../pageObject/login.js";
+import QuickReport from "../../../pageObject/quick_report.js";
+const quickReport = new QuickReport();
+const loginPage = new LoginPage()
+
+describe('Portal Downloads : quick reports  ', () => {
+    before( () =>{ 
+        cy.fixture('loginPage.json').then((userdata)=>{
+            cy.visit(Cypress.env('url'))
+            loginPage.getEmailInputField(userdata.adminEmail);
+            loginPage.getPasswordInputField(userdata.adminPassword);
+            loginPage.getsubmitBtn();
+            loginPage.getTextVerifyLogin();
+        })
+    })
+
+    it('should show confirmation message of quick report  after clicking on download for button of This report is used for MRM with this month',()=>{
+        cy.wait(5000)
+        quickReport.getClickOnDownloadCenter();
+        quickReport.getClickOnQuickReport();
+        quickReport.verifyTitleText().eq(19).should("contain.text", "This report is used for MRM")
+        quickReport.getClickOnDatePicker().eq(15).click();
+        quickReport.getClickOnThisMonth();
+        quickReport.getClickOnDownloadForBtn().eq(15).click();
+        quickReport.verifySuccessMessage();
+        
+    })
+})
