79a9151d9eb3dbe5773a239fe077319e2aaf62e1
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-04-29 14:34:53 +0530

    added multiple columns description quick report

---
+++ cypress/e2e/reportDownloads/multipleColumnsDescriptionQuickReport/multiple_columns_description_report_of_last_week.cy.js
@@ -0,0 +1,28 @@
+import LoginPage from "../../../pageObject/login.js";
+import QuickReport from "../../../pageObject/quick_report.js";
+const quickReport = new QuickReport();
+const loginPage = new LoginPage()
+
+describe('Portal Downloads : quick reports  ', () => {
+    before( () =>{ 
+        cy.fixture('loginPage.json').then((userdata)=>{
+            cy.visit(Cypress.env('url'))
+            loginPage.getEmailInputField(userdata.adminEmail);
+            loginPage.getPasswordInputField(userdata.adminPassword);
+            loginPage.getsubmitBtn();
+            loginPage.getTextVerifyLogin();
+        })
+    })
+
+    it('should show confirmation message of quick report  after clicking on download for button of Multiple Columns Description with last week',()=>{
+        cy.wait(5000)
+        quickReport.getClickOnDownloadCenter();
+        quickReport.getClickOnQuickReport();
+        quickReport.verifyTitleText().eq(16).should("contain.text", "Multiple Columns Description")
+        quickReport.getClickOnDatePicker().eq(12).click();
+        quickReport.getClickOnLastWeek();
+        quickReport.getClickOnDownloadForBtn().eq(12).click();
+        quickReport.verifySuccessMessage();
+        
+    })
+})
