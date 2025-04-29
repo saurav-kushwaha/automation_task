912178e07c9dc0557bf603d8d9a208a52fef3178
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-04-29 14:58:06 +0530

    added default invite quick report

---
+++ cypress/e2e/reportDownloads/defaultInviteQuickReport/default_invite_report_of_last_month.cy.js
@@ -0,0 +1,28 @@
+import LoginPage from "../../../pageObject/login.js";
+import QuickReport from "../../../pageObject/quick_report.js";
+const quickReport = new QuickReport();
+const loginPage = new LoginPage()
+
+describe('Portal Downloads : quick reports  ', () => {
+    before( () =>{ 
+        cy.fixture('loginPage.json').then((userdata)=>{
+            cy.visit(Cypress.env('url'))
+            loginPage.getEmailInputField(userdata.adminEmail);
+            loginPage.getPasswordInputField(userdata.adminPassword);
+            loginPage.getsubmitBtn();
+            loginPage.getTextVerifyLogin();
+        })
+    })
+
+    it('should show confirmation message of quick report  after clicking on download for button of Default Invite Report with last month',()=>{
+        cy.wait(5000)
+        quickReport.getClickOnDownloadCenter();
+        quickReport.getClickOnQuickReport();
+        quickReport.verifyTitleText().eq(18).should("contain.text", "Default Invite Report")
+        quickReport.getClickOnDatePicker().eq(14).click();
+        quickReport.getClickOnLastMonth();
+        quickReport.getClickOnDownloadForBtn().eq(14).click();
+        quickReport.verifySuccessMessage();
+        
+    })
+})
